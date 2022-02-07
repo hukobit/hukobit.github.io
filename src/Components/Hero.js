f620682bc7561dd9b5e204d094778fd6ded48fdb
hukobit <95817300+hukobit@users.noreply.github.com>
2022-02-08 00:05:42 +0300

    fix

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -5,8 +5,8 @@ export default class Hero extends Component {
     return  <div>
                 <div className='p-5 mb-4 rounded-3' style={{backgroundImage: 'url("https://cutewallpaper.org/21/seamless-space-background/Starrs-In-Outer-Space-Seamless-Background-Or-Texture-.jpg")'}}>
                     <div className='container-fluid py-5'>
-                        <h4 className='display-8 fw-bold fst-italic text-white text-center'>Konumsal Bilgi Topluluğu'na Hoş Geldiniz</h4>
-                        <p className='fs-4 fw-light font-italic text-white text-center'>
+                        <h4 className='display-8 fw-bold text-white text-center'>Konumsal Bilgi Topluluğu'na Hoş Geldiniz</h4>
+                        <p className='fs-4 fw-light fst-italic text-white text-center'>
                           Kendinizi geliştirebileceğiniz, gelişirken eğlenebileceğiniz bir topluluk mu arıyorsunuz? Öyleyse doğru yerdesiniz. 
                         </p>
 
